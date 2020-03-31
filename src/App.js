4cf50fcf247304aa8cd6501c532f2af7d65b6e8c
mtodoroff <martin.vladimirov.todorov@gmail.com>
2020-03-31 15:40:54 +0300

    updated menu-items with HOC withRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import {Route,Switch} from 'react-router-dom';
 import HomePage from './pages/homepage.component';
 
+
 function App() {
+
+  const HatsPage = () => (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  )
+
   return (
     <div >
-        <HomePage />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/hats" component={HatsPage}/>
+        </Switch>
+        
     </div>
   );
 }
