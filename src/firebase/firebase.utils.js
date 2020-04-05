00d79901f5343b77119f15588f0bf6ff9a4c046d
mtodoroff <martin.vladimirov.todorov@gmail.com>
2020-04-05 20:06:33 +0300

    add reselect replace cart selectors with memoized selectors

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,21 +1,18 @@
-
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCV_AWG2mPVPndimXYWwuuwJW9IRU2i0lw",
-  authDomain: "e-commdb-33148.firebaseapp.com",
-  databaseURL: "https://e-commdb-33148.firebaseio.com",
-  projectId: "e-commdb-33148",
-  storageBucket: "e-commdb-33148.appspot.com",
-  messagingSenderId: "769978115808",
-  appId: "1:769978115808:web:9a2ab0c3d64fe32e581285",
-  measurementId: "G-Q5M5BXYEG1"
+const config = {
+  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
+  authDomain: 'crwn-db.firebaseapp.com',
+  databaseURL: 'https://crwn-db.firebaseio.com',
+  projectId: 'crwn-db',
+  storageBucket: 'crwn-db.appspot.com',
+  messagingSenderId: '850995411664',
+  appId: '1:850995411664:web:7ddc01d597846f65'
 };
 
-firebase.initializeApp(firebaseConfig);
-
+firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
